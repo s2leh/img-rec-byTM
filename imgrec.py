588fa3932a1aac7f509a4ca68191b3480c208d49
s2leh <reke112@gmail.com>
2025-07-03 23:59:23 +0300

    imgrec.py

--- imgrec.py
+++ imgrec.py
@@ -1,31 +1,27 @@
 from keras.models import load_model  # TensorFlow is required for Keras to work
-from PIL import Image, ImageOps  # Install pillow instead of PIL
+from PIL import Image, ImageOps      # Install pillow (not PIL)
 import numpy as np
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = load_model(r"C:\Users\reke1\OneDrive\Desktop\image recognition model\keras_Model.h5", compile=False)
-
+model = load_model("keras_Model.h5", compile=False)
 
 # Load the labels
-class_names = open(r"C:\Users\reke1\OneDrive\Desktop\image recognition model\labels.txt", "r").readlines()
-
+class_names = open("labels.txt", "r").readlines()
 
 # Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Replace this with the path to your image
-image = Image.open("C:/Users/reke1/OneDrive/Desktop/image recognition model/Screenshot_5.png").convert("RGB")
+image = Image.open("a Banana.png").convert("RGB")
 
-# resizing the image to be at least 224x224 and then cropping from the center
+# Resize the image to be 224x224 and crop from the center
 size = (224, 224)
 image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-# turn the image into a numpy array
+# Convert the image to a numpy array
 image_array = np.asarray(image)
 
 # Normalize the image
@@ -34,12 +30,12 @@ normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 # Load the image into the array
 data[0] = normalized_image_array
 
-# Predicts the model
+# Predict using the model
 prediction = model.predict(data)
 index = np.argmax(prediction)
-class_name = class_names[index]
+class_name = class_names[index].strip()
 confidence_score = prediction[0][index]
 
 # Print prediction and confidence score
-print("Class:", class_name[2:], end="")
+print("Class:", class_name)
 print("Confidence Score:", confidence_score)
